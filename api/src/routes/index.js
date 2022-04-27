aa3cf92a0956877baa39e217b0663f7bc9e2b5e4
Eugenia Nannini <euge.nannini@gmail.com>
2022-04-27 11:20:30 -0300

    nuevos cambios

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,17 +8,15 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-//-------------------faltaria relacionar la actividad que estoy creando a quien??? calendario?? 
+ 
 router.post("/activity", async (req,res) => {
     try{
         const{ name, description, video, image, price, day, hour, capacity } = req.body
         const newAct = await Activity.create({name, description, video, image, price, day, hour, capacity }) 
-        //tengo que agregar la nueva actividad al calendario (??
         res.send (newAct)
     
     } catch(err){
-        res.send(err)
+        console.log(err)
     }
 })
 
