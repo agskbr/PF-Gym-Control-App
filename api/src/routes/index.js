f08718458bcea0450fe98a3b9e9f39966d654846
matiasgonz1653 <96365031+matiasgonz1653@users.noreply.github.com>
2022-04-26 23:12:48 -0300

    ruta /users/:id

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,5 +8,13 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get("/users/:id", async (req, res) => {
+    const { id } = req.params;
+    if (id) {
+        const userId = await getDetailsUser()
+        ? res.status(200).send(userId)
+        : res.status(404).send("Perro no encontrado");
+    }
+});
 
 module.exports = router;
