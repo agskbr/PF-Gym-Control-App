2abf1cb158991f8a30e4ee519c8b76bd9e4c531b
matiasgonz1653 <96365031+matiasgonz1653@users.noreply.github.com>
2022-04-26 23:31:00 -0300

    funcion getDetailsUser dentro del controller

--- api/src/Controller/users.js
+++ api/src/Controller/users.js
@@ -2,3 +2,25 @@ const {
     Activity,
     User
 } = require('../db');
+
+const getDetailsUser = async (id) => {
+    const user = await User.findAll({
+        where: {
+            name: id
+        },
+        include: {
+            model: Activity,
+            attributes: ["name"],
+            through: {
+                attributes: [],
+            }
+        }
+    })
+
+    return user;
+}
+
+
+module.exports = {
+    getDetailsUser
+}
