fd32a185b39533eeb555e65094ddb7c68144fc07
gadeangelis <gadeangelis@gmail.com>
2022-04-27 10:11:09 -0300

    get de recetas
    Creo carpeta de controladores y rutas para las recetas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,11 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
-
 const router = Router();
+
+const recipes = require ('./Routes/Recipes')
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use('/recipes', recipes)
 
 module.exports = router;
