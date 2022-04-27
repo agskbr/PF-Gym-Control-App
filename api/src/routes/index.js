f06896f248201777fba081311d01ce89cfcaad36
Eugenia Nannini <euge.nannini@gmail.com>
2022-04-26 23:35:43 -0300

    Relacion + ruta post
    revisar ruta post

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const Activity = require('../models/Activity');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,19 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+//-------------------faltaria relacionar la actividad que estoy creando a quien??? calendario?? 
+router.post("/activity", async (req,res) => {
+    try{
+        const{ name, description, video, image, price, day, hour, capacity } = req.body
+        const newAct = await Activity.create({name, description, video, image, price, day, hour, capacity }) 
+        //tengo que agregar la nueva actividad al calendario (??
+        res.send (newAct)
+    
+    } catch(err){
+        res.send(err)
+    }
+})
 
 
 module.exports = router;
