4af81fc03bec4f0c0cd7d86c2a102c2a12f24610
gadeangelis <gadeangelis@gmail.com>
2022-04-27 10:42:28 -0300

    rutas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 const router = Router();
 
 const recipes = require ('./Routes/Recipes')
+const user = require ('./Routes/User')
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.use('/recipes', recipes)
+router.use ('/user', user)
 
 module.exports = router;
