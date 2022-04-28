1f08c15f503449143712121a3957d0fc210c5ee8
Agustin Cabrera <cabreraag01@gmail.com>
2022-04-28 07:14:34 -0300

    Renderizo el componente TopBar en la pagina del administrador

--- client/src/views/AdminPage/AdminPage.jsx
+++ client/src/views/AdminPage/AdminPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import style from "./AdminPage.module.css";
 import SideBar from "./components/SideBar/SideBar.jsx";
+import TopBar from "./components/TopBar/TopBar";
 
 export default function AdminPage() {
   return (
     <div className={style.principalContainer}>
       <SideBar />
+      <TopBar />
     </div>
   );
 }
