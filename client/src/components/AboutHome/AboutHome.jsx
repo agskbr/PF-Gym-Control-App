1a0d04897b0200986e5a82bc0e481d1ca05c822b
Yanina <yaninagarcia.ana@gmail.com>
2022-04-29 20:22:17 -0300

    conectando eÃl componente About con la BD

--- client/src/components/AboutHome/AboutHome.jsx
+++ client/src/components/AboutHome/AboutHome.jsx
@@ -1,45 +1,21 @@
-import React from 'react'; 
+import React, { useEffect } from 'react'; 
+import {useDispatch, useSelector} from 'react-redux';
+import { getAllTrainers } from '../../store/actions';
 import s from './AboutHome.module.css';
 //import video from '../../assets/180419_Boxing_A1_04.mp4';
-import t1 from '../../assets/trainer1.jpg';
-import t2 from '../../assets/trainer2.jpg';
-import t3 from '../../assets/trainer3.jpg';
-import t4 from '../../assets/trainer4.jpg';
 import Trainers from '../Trainers/Trainers';
 
 
 
 export default function AboutHome() {
-    const entrenadores = [
-        {
-            id: 1,
-            nombre: "Carolina Fernandez",
-            img: t1,
-            especialidad: "Yoga",
-            experiencia: 6
-        },
-        {
-            id:2,
-            nombre: "Valeria Odon",
-            img: t2,
-            especialidad: "Personal Trainer",
-            experiencia: 4
-        },
-        {
-            id:3,
-            nombre: "Carlos Alarcon",
-            img: t3,
-            especialidad: "Personal Trainer",
-            experiencia: 8
-        },
-        {
-            id:4,
-            nombre: "Gaston Garcia",
-            img: t4,
-            especialidad: "Box",
-            experiencia: 5
-        }
-    ]
+  const dispatch = useDispatch();
+  const entrenadores = useSelector((state)=> state.trainers)
+  console.log(entrenadores)
+
+  useEffect(()=>{
+      dispatch(getAllTrainers())
+  },[dispatch])
+
   return (
     <main>
         <div className={s.aboutPage}>
@@ -67,10 +43,10 @@ export default function AboutHome() {
                     <div className={s.flexy}>
                         {entrenadores.map(e =>
                         <Trainers key={e.id}
-                            nombre={e.nombre}
-                            img={e.img}
-                            especialidad={e.especialidad}
-                            experiencia={e.experiencia}
+                            name={e.name}
+                            image={e.image}
+                            specialty={e.specialty}
+                            experience={e.experience}
                         />)}
                     </div>
                 </section>
