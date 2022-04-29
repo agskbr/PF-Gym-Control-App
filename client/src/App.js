3bce4e7440d8a2627c367fe86e7b6cf4a35f9a40
YaniAnaGarcia <yaninagarcia.ana@gmail.com>
2022-04-29 08:15:07 -0300

    cambios en app

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AdminPage from "./views/AdminPage/AdminPage";
 
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<h1>Proyecto final Henry</h1>} />
         <Route path="/adminDashboard" element={<AdminPage />} />
+        
       </Routes>
     </div>
   );
