4741e12dc863c4c761c05e30de9e27ecc695236c
Santi <juan_santiago@outlook.com>
2022-04-29 20:30:01 -0300

    Se le da estilos al componente Clases para mostrar la info relevante y un display grid para las cards de activities

--- client/src/components/Clases/Clases.jsx
+++ client/src/components/Clases/Clases.jsx
@@ -7,9 +7,12 @@ return (
 
     <div className={style.container}>
         
+        <div className={style.info}>
+            
+        </div>
+        
         <div className={style.clases}>
-
-
+            
         </div>
 
     </div>
