6624612629acc52b42e42b27e3bbc9197df99f80
Agustin Cabrera <cabreraag01@gmail.com>
2022-04-29 10:22:49 -0300

    Agrego accion para despachar en la creacion de actividades

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,2 +1,14 @@
+import axios from "axios";
 // import {} from "../actions-type/index";
 
+const createActivity = (activity) => {
+  return async () => {
+    try {
+      await axios.post("http://localhost:3001/activity", activity);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export { createActivity };
