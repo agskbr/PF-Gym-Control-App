342e39edf34fe58fd732235d3720f5279417717b
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-04-30 11:03:33 -0300

    Estructura basica de la tabla que muestra las actividades y permitirá editar las mismas

--- client/src/views/AdminPage/components/AdminCardView/AdminCardView.jsx
+++ client/src/views/AdminPage/components/AdminCardView/AdminCardView.jsx
@@ -4,19 +4,81 @@ import style from "./AdminCardView.module.css";
 export default function AdminCardView() {
   return (
     <div className={style.principalContainer}>
-      <h4>Actividades</h4>
-      <button onClick={() => document.getElementById("dialogId").showModal()} className={style.addButton}>Agregar</button>
-      <div className={style.card}>
-        <div className={style.headerOfCard}>
-          <div className={style.header}>ID</div>
-          <div className={style.header}>Nombre</div>
-          <div className={style.header}>Descripcion</div>
-          <div className={style.header}>Price</div>
-          <div className={style.header}>Image</div>
-          <div className={style.header}>Dia</div>
-          <div className={style.header}>Horario</div>
-          <div className={style.header}>Capacidad</div>
-        </div>
+      <div className={style.titleAndAddBtn}>
+        <h4>Actividades</h4>
+        <button
+          onClick={() => document.getElementById("createDialog").showModal()}
+          className={style.addBtn}
+        >
+          Agregar
+        </button>
+      </div>
+      <div className={style.cardLayout}>
+        <table className={style.tableAdminView}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th className={style.thDescription}>Description</th>
+              <th>Price</th>
+              <th>Video</th>
+              <th>Image</th>
+              <th>Days</th>
+              <th>Hours</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2</td>
+              <td>Zumba</td>
+              <td className={style.tdDescription}>
+                El éxito de estas clases reside en que no es necesario saber
+                bailar para poder participar. A través de la combinación de
+                pasos sencillos y generalmente relacionados con los ritmos
+                latinos y el aerobic, se crean coreografías fáciles y divertidas
+                con las que conseguimos quemar calorías pasando un rato
+                divertido. En el caso del Zumba, se incluyen ejercicios de
+                fitness durante las coreografías, permitiendo que las clases
+                sean un poco más intensas. Ayuda a educir el peso, mejorar el
+                equilibrio y reducir el estrés.
+              </td>
+              <td>200</td>
+              <td className={style.tdImageAndVideo}>
+                "https://time2feat.com/wp-content/uploads/2019/11/mejores-clases-colectivas-gimnasio-en-grupo-ejercicio-entrenamiento-efectivas-zumba.jpg.webp",
+              </td>
+              <td className={style.tdImageAndVideo}>
+                "https://youtu.be/vYXRZNVVFVA"
+              </td>
+              <td>
+                <div className={style.listOfItems}>
+                  <ul>
+                    <li>Lunes</li>
+                    <li>Miercoles</li>
+                    <li>Viernes</li>
+                  </ul>
+                </div>
+              </td>
+              <td>
+                <div className={style.listOfItems}>
+                  <ul>
+                    <li>14-16</li>
+                    <li>18-20</li>
+                  </ul>
+                </div>
+              </td>
+              <td>
+                <button
+                  onClick={() =>
+                    document.getElementById("editDialog").showModal()
+                  }
+                  className={style.editBtn}
+                >
+                  Edit
+                </button>
+              </td>
+            </tr>{" "}
+          </tbody>
+        </table>
       </div>
     </div>
   );
