2a42e9587619c977436463bd4115d682782dad03
matiasgonz1653 <96365031+matiasgonz1653@users.noreply.github.com>
2022-04-30 18:08:07 -0300

    upd

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DataTypes } = require("sequelize");
 const {DB_USER, DB_PASSWORD, DB_HOST ,DB_NAME} = process.env;
 
 let sequelize =
