ce17ff1ba7f2947ff4d57de71e37fd3b3bc9ad52
Santi <juan_santiago@outlook.com>
2022-05-02 12:00:07 -0300

    modificacion de iconos

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
         <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/index.html" element={<HomePage />} />
         <Route path="/clases/:id" element={<Buy />} />
         <Route exact path="/adminDashboard" element={<AdminPage />} />
         <Route
