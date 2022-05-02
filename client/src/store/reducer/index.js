49fe89f41078c50b03b8bdc6d93594c7491bda9e
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-05-02 08:32:23 -0300

    Borro unas lineas repetidas, falta solucionar un error que me da con las actividades

--- client/src/store/reducer/index.js
+++ client/src/store/reducer/index.js
@@ -43,13 +43,6 @@ const rootReducer = (state = initialState, action) => {
         detail: action.payload,
       };
   
-      case 'GET_ACTIVITY_DETAIL':
-        console.log(action.payload);
-        return {
-          ...state,
-          detail: action.payload,
-        };
-
       case 'SEARCH_BY_NAME':
           return {
             ...state, 
