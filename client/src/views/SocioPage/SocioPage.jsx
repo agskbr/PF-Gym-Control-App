e76c99bf8face5bce373b2609c7d26321c1411e5
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-05-06 07:28:41 -0300

    Despachando accion que valida si el usuario tiene una sesión activa, sino redirecciona a page inicial

--- client/src/views/SocioPage/SocioPage.jsx
+++ client/src/views/SocioPage/SocioPage.jsx
@@ -1,22 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { userSignOut } from "../../store/actions/actions-login";
+import {
+  userSignOut,
+  validateUserIsLogged,
+} from "../../store/actions/actions-login";
 import style from "./SocioPage.module.css";
 
 export default function SocioPage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { user } = useSelector((state) => state.pgym);
+  const { user } = useSelector((state) => state.login);
+
   useEffect(() => {
-    if (!user) {
+    dispatch(validateUserIsLogged());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (user === null) {
       navigate("/");
     }
   }, [user, navigate]);
+
   return (
     <div className={style.principalContainer}>
       <h2>SocioPage</h2>
-      <button className={style.signOutBtn} onClick={() => dispatch(userSignOut())}>SignOut</button>
+      <button
+        className={style.signOutBtn}
+        onClick={() => dispatch(userSignOut())}
+      >
+        SignOut
+      </button>
     </div>
   );
 }
