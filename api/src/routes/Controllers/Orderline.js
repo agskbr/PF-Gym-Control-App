d1d1bee549c85b210e38095297ad4f2509138940
Juan Ignacio <93682709+juani029@users.noreply.github.com>
2022-05-06 17:35:43 -0300

    upd

--- api/src/routes/Controllers/Orderline.js
+++ api/src/routes/Controllers/Orderline.js
@@ -1,15 +1,15 @@
-const {Orderline} = require('../../db')
+const {OrderLine} = require('../../db')
 
 const createOrderline = async(unitprice, subtotal, quantity, orderId, activityId) =>{
     try {
-        const orderline = await Orderline.findOne({
+        const orderline = await OrderLine.findOne({
             where: {
                 orderId: orderId,
                 activityId:activityId
             },
         })
         if(!orderline){
-            const newOrderline = await Orderline.create({
+            const newOrderline = await OrderLine.create({
                 unitprice,
                 subtotal,
                 quantity,
@@ -27,7 +27,7 @@ const createOrderline = async(unitprice, subtotal, quantity, orderId, activityId
 
 const orderlineByOrderId = async(orderId) =>{
     try {
-        const orderlineId = await Orderline.findAll({
+        const orderlineId = await OrderLine.findAll({
             where:{
                 orderId: orderId
             }
@@ -38,7 +38,21 @@ const orderlineByOrderId = async(orderId) =>{
     }
 }
 
+const orderlineByActivityId = async(activityId) =>{
+    try {
+        const orderlineActId = await OrderLine.findAll({
+            where:{
+                activityId: activityId
+            }
+        })
+        return orderlineActId
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
     createOrderline,
-    orderlineByOrderId
+    orderlineByOrderId,
+    orderlineByActivityId
 }
