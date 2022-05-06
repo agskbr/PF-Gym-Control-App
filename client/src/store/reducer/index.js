03f1193bc661df943024c22c31f894a48377938d
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-05-06 07:26:36 -0300

    Agrego loginReducer al combineReducers

--- client/src/store/reducer/index.js
+++ client/src/store/reducer/index.js
@@ -1,10 +1,12 @@
-import {combineReducers} from 'redux';
-import { rootReducer } from './rootReducer';
-import { cartReducer } from './cartReducer';
+import { combineReducers } from "redux";
+import { rootReducer } from "./rootReducer";
+import { cartReducer } from "./cartReducer";
+import { loginReducer } from "../reducer/login-reducer";
 
 const reducer = combineReducers({
-    pgym: rootReducer,
-    cart: cartReducer,
+  pgym: rootReducer,
+  cart: cartReducer,
+  login: loginReducer,
 });
 
 export default reducer;
