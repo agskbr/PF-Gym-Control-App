1e5eb1a81560226a5985a7aa5f3c2281b39fc035
Yanina <yaninagarcia.ana@gmail.com>
2022-05-05 22:20:08 -0300

    actualizando actions-review y creacion de componente Createreview

---
+++ client/src/store/actions/actions-review.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import {CREATE_REVIEW, GET_ALL_REVIEWS,} from '../actions-type/index';
+
+const base_url = "http://localhost:3001";
+
+export const postReview = (review)=>{ // review = description, rating, userId, img??
+    return async (dispatch)=> {
+        try {
+            const {data} = await axios.post(`${base_url}/reviews`, review)
+            return dispatch({
+                type:CREATE_REVIEW,
+                payload:data
+            })
+        } catch (err) {
+            console.log('postReview error', err)
+        }
+    }
+}
+
+export const getAllReviews = ()=> {
+    return async (dispatch)=> {
+        try {
+            const {data} = await axios.get(`${base_url}/reviews`)
+            return dispatch({
+                type: GET_ALL_REVIEWS,
+                payload: data
+            })
+        } catch (err) {
+            console.log('error en getAllReviews', err)
+        }
+    }
+}
