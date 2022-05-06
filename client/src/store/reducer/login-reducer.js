f842a7ba00315c75ad4b61582008b18b73219562
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-05-06 08:09:12 -0300

    Agregando reducer para login, registro y cierre de sesion del usuario, tambien para verificar si tiene una sesion activa

---
+++ client/src/store/reducer/login-reducer.js
@@ -0,0 +1,43 @@
+import {
+  LOGIN_WITH_GOOGLE,
+  REGISTER_USER_WITH_EMAIL_AND_PASS,
+  USER_SIGN_OUT,
+  VALIDATE_USER_IS_LOGGED,
+} from "../actions-type";
+
+const initialState = {
+  user: {},
+};
+
+const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_WITH_GOOGLE:
+      return {
+        ...state,
+        user: action.payload,
+      };
+
+    case REGISTER_USER_WITH_EMAIL_AND_PASS:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case USER_SIGN_OUT:
+      return {
+        ...state,
+        user: null,
+      };
+    case VALIDATE_USER_IS_LOGGED:
+      return {
+        ...state,
+        user: action.payload,
+      };
+
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export { loginReducer };
