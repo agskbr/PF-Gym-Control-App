396402c47e8d4f8778175d04a78cbb7f3f0dcd91
Yanina <yaninagarcia.ana@gmail.com>
2022-05-07 19:37:30 -0300

    actualizando componentes centerDynamicView sideBar y sociopage

--- client/src/views/SocioPage/SocioPage.jsx
+++ client/src/views/SocioPage/SocioPage.jsx
@@ -1,26 +1,21 @@
 import style from "./SocioPage.module.css";
 import SideBar from "./components/Sidebar/SideBar";
 import CenterDynamicView from "./components/CenterDynamicView/CenterDynamicView";
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { validateUserIsLogged, userSignOut } from "../../store/actions/actions-login";
-=======
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { validateUserIsLogged , userSignOut} from "../../store/actions/actions-login";
+import MyActivities from "./components/MyActivities/MyActivities";
 
->>>>>>> main
+
 
 export default function SocioPage() {
 
   const [itemSelected, setItemSelected] = useState("Mi Perfil")
   const dispatch = useDispatch();
-  const navigate = useNavigate
+  const navigate = useNavigate();
   const { user } = useSelector((state)=> state.login);
-  
+  const [componentToRender, setComponentToRender]= useState("")
   useEffect(() => {
     dispatch(validateUserIsLogged());
   }, [dispatch]);
@@ -31,27 +26,26 @@ export default function SocioPage() {
     }
   }, [user, navigate]);
 
+  
+
   return (
     <div className={style.principalContainer}>
-     {/*  <SideBar itemSelected={itemSelected} setItemSelected={setItemSelected} /> */}
+      <SideBar itemSelected={itemSelected} setItemSelected={setItemSelected} />
       <div className={style.centerViewContainer}>
-        <button
+        {/* <button
           className={style.signOutBtn}
           onClick={() => dispatch(userSignOut())}
         >
           SignOut
         </button>
-        <br />
-        {/* <CenterDynamicView
+        <br /> */}
+        <CenterDynamicView
           itemSelected={itemSelected}
           contentOfCard={<div>Hola</div>}
-        /> */}
+        />
       </div>
 
-      {/*  <div>
-        <MyActivities />
-      </div>
-      <CreateReaview/> */}
+     
     </div>
   );
 }
