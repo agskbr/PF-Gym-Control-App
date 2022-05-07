5b3d9b9f6a17f0193ff2deac7af9fff34ac5f778
Santi <juan_santiago@outlook.com>
2022-05-07 08:56:36 -0300

    se crea componente CartItem que remplazara a ProductItem

---
+++ client/src/components/CartItem/CartItem.jsx
@@ -0,0 +1,33 @@
+import style from './cartItem.module.css';
+
+const CartItem = ({data, removeFromCart, info}) => {
+   const {
+         id,
+            name,
+            image,
+            price,
+            day,
+            hour,
+            capacity,
+            quantity
+   } = data;
+
+
+
+    return (
+        <div className={style.container}>
+            <div className={style.contentContainer}>
+                <h4>{name}</h4>
+                <h4>${price}.00</h4>
+                <h5>x{quantity}</h5>
+                <h6>Total: ${price * quantity}.00</h6>
+            </div>
+            <div className={style.buttonsContainer}>
+                <button onClick={() => removeFromCart(id)}></button>
+                {/* <button onClick={() => removeFromCart(id, true)}>del all</button> */}
+            </div>
+        </div>
+    );
+}
+
+export default CartItem;
