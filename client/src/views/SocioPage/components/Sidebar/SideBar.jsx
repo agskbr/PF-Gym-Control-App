ab223bb22ca2897a35fb2e77f9a319f20583e675
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-05-07 16:57:56 -0300

    Cambio el navLink por un 'a' y le seteo la classname active de acuerdo al itemSelected

--- client/src/views/SocioPage/components/Sidebar/SideBar.jsx
+++ client/src/views/SocioPage/components/Sidebar/SideBar.jsx
@@ -1,69 +1,86 @@
-import React, { useState } from 'react';
-import s from './SideBar.module.css';
-import {AiFillHome} from 'react-icons/ai';
-import {CgGym} from 'react-icons/cg'; 
-import {GiChickenOven} from 'react-icons/gi';
-import {FaUserCircle} from 'react-icons/fa';
-import {BsCartFill} from 'react-icons/bs';
-import {GoSignOut} from 'react-icons/go'
-import logo from '../../../../assets/logo.png'
-import {NavLink} from 'react-router-dom'
+import React, { useState } from "react";
+import s from "./SideBar.module.css";
+import { AiFillHome } from "react-icons/ai";
+import { CgGym } from "react-icons/cg";
+import { GiChickenOven } from "react-icons/gi";
+import { FaUserCircle } from "react-icons/fa";
+import { BsCartFill } from "react-icons/bs";
+import { GoSignOut } from "react-icons/go";
+import logo from "../../../../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
-
-export default function SideBar(){
-  const [isOpen, setIsOpen]= useState(false);
-  const toggle= ()=> setIsOpen(!isOpen);
+export default function SideBar({ itemSelected, setItemSelected }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
   const menuItem = [
     {
-      icon: <FaUserCircle/>,
-      name:"Mi Perfil",
-      path:"/miperfil",
+      icon: <FaUserCircle />,
+      name: "Mi Perfil",
+      path: "#/miperfil",
     },
     {
-        name:"Home",
-        icon: <AiFillHome/>,
-        path:"/",
+      name: "Home",
+      icon: <AiFillHome />,
+      path: "#/",
     },
     {
-        name:"Actividades",
-        icon: <CgGym/>,
-        path:"/myActivities",
+      name: "Actividades",
+      icon: <CgGym />,
+      path: "#/myActivities",
     },
     {
-        name:"Recetas",
-        icon: <GiChickenOven/>,
-        path:"/Recetas",
+      name: "Recetas",
+      icon: <GiChickenOven />,
+      path: "#/Recetas",
     },
     {
-        name:"Compras",
-        icon: <BsCartFill/>,
-        path:"/miscompras",
+      name: "Compras",
+      icon: <BsCartFill />,
+      path: "#/miscompras",
     },
-    {
-        name:"SignOut",
-        icon: <GoSignOut/>,
-        path:"/",
-    }
-  ]
+    // {
+    //   name: "SignOut",
+    //   icon: <GoSignOut />,
+    //   path: "/",
+    // },
+  ];
   return (
     <div className={s.sideBarUserContainer}>
-      <div style={{width: isOpen ? "250px" : "50px"}} className={s.sideBarUser}>
+      <div
+        style={{ width: isOpen ? "250px" : "50px" }}
+        className={s.sideBarUser}
+      >
         <div className={s.sideBarLogo}>
           <img src={logo} alt="logo" width={30} onClick={toggle} />
-            <h1 style={{display: isOpen ? "block" : "none"}} className={s.sideBarTitle}>Gym Control</h1>
+          <h1
+            style={{ display: isOpen ? "block" : "none" }}
+            className={s.sideBarTitle}
+          >
+            Gym Control
+          </h1>
         </div>
-        {
-          menuItem.map((item, index)=>(
-            <NavLink to={item.path} key={index} className={s.sideBarLink} activeClassname="active">
-              <div className={s.sideBarIcon}>{item.icon}</div>
-              <div style={{display: isOpen ? "block" : "none"}} className={s.sideBarText}>{item.name}</div> 
-            </NavLink>
-          ))
-        }
+        {menuItem.map((item, index) => (
+          <a
+            href={item.path}
+            onClick={() => setItemSelected(item.name)}
+            key={index}
+          >
+            <div
+              className={itemSelected === item.name ? s.active : s.sideBarIcon}
+            >
+              {item.icon}
+            </div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className={s.sideBarText}
+            >
+              {item.name}
+            </div>
+          </a>
+        ))}
       </div>
       {/* <main>{children}</main> */}
     </div>
-  )
+  );
 }
-
