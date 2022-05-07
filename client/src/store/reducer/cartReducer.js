abe49a06d5fc7f34322922ed863b8f895f12bc67
Santi <juan_santiago@outlook.com>
2022-05-07 09:02:24 -0300

    se actualizan los reducers del Cart

--- client/src/store/reducer/cartReducer.js
+++ client/src/store/reducer/cartReducer.js
@@ -4,39 +4,103 @@ import { ADD_TO_CART, REMOVE_ONE_FROM_CART, REMOVE_ALL_FROM_CART, CLEAR_CART } f
 
 
 
-export const cartInitialState = {
+export const initialState = {
     products: [],
     cart: [],
+    totalCart: 0,
 }
 
 
-export function cartReducer(state = cartInitialState, action) {
+export function cartReducer(state = initialState, action) {
     switch (action.type) {
         case ADD_TO_CART:
-            return {
-                ...state,
-                products: state.products,
-                cart: [...state.cart, action.payload]
+            let product = state.cart.find(product => product.id === action.payload.id);
+
+           /*  console.log(action.payload) */
+            /* console.log(product);  */     
+            
+            let itemInCart = state.cart?.find(item => item.id === action.payload.id);
+            /* console.log(state.cart);
+            console.log(itemInCart); */
+
+            if (itemInCart) {
+                itemInCart.quantity += 1;
+                return {
+                    ...state,
+                    cart: [...state.cart],
+                }
+            } else {
+                return {
+                    ...state,
+                    cart: [...state.cart, { ...action.payload, quantity: 1 }],
+                }
             }
+            
+            /* return itemInCart ? {...state,
+                                cart: state.cart.map(item =>
+                                    item.id === product.id 
+                                ? {...item, quantity: item.quantity + 1}
+                                : item
+                                )} : {
+                                    ...state,
+                                    cart: [...state.cart, {...action.payload, quantity: 1}]}; */
+
         case REMOVE_ONE_FROM_CART:
-            return {
-                ...state,
-                products: state.products,
-                cart: state.cart.filter(product => product.id !== action.payload)
-            }
+            let itemInCartToDel = state.cart.find(item => item.id === action.payload);
+
+            return itemInCartToDel.quantity > 1 ? {...state,
+                                cart: state.cart.map(item =>
+                                item.id === action.payload
+                                ? {...item, quantity: item.quantity - 1}
+                                : item
+                                )} : {
+                                    ...state,
+                                    cart: state.cart.filter(item => item.id !== action.payload)};
+
+       /*  case ADD_TO_CART:{
+            if (state.cart.length > 0) {
+                let isProduct = state.cart.filter((e) => e.id === action.payload.id);
+                
+                if (isProduct.length > 0) {
+                  let finalProduct = state.cart.map((e) => {
+                    if (e.id === action.payload.id) {
+                      e.count += 1;
+                      return e;
+                    } else {
+                      return e;
+                    }
+                  });
+                  return finalProduct;
+                } else {
+                  let cart = state.cart;
+                  action.payload.quantity = 1;
+                  cart.push(action.payload);
+                  return cart;
+                }
+              } else {
+                let cart = [];
+                action.payload.quantity = 1;
+                cart.push(action.payload);
+                return cart;
+              }
+
+        } */
+
+            
+        
         case REMOVE_ALL_FROM_CART:
-            return {
-                ...state,
-                products: state.products,
-                cart: []
-            }
+            return state.cart.filter(item => item.id !== action.payload);
         case CLEAR_CART:
+                return {
+                    cart : state.cart,
+                }
+
+        case TOTAL_CART:
             return {
                 ...state,
-                products: state.products,
-                cart: []
+                totalCart: action.payload
             }
-        default:
-            return state;
+              default:
+                return state;
     }
 }
