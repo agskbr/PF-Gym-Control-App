c2939116a481d2772cfe6f5ef6e6ab420fece7c7
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2022-05-08 14:40:18 -0400

    Cart funcionando
    Funcionalidad de click en cart funciona

--- client/src/components/Nav/nav.jsx
+++ client/src/components/Nav/nav.jsx
@@ -8,6 +8,7 @@ import Cart from "../Cart/Cart";
 export default function Nav() {
   let [headerClasses, setHeaderClasses] = useState([]);
   let [scrollPosition, setScrollPosition] = useState(0);
+  let [cartClass, setCartClass] = useState('cart-closed')
 
   useState(() => {
     setHeaderClasses(classNames("header"));
@@ -23,6 +24,11 @@ export default function Nav() {
       return setHeaderClasses((headerClasses = ["headerScrolled"]));
     }
   };
+
+  function handleCartClick() {
+    if (cartClass === 'cart-closed') setCartClass('cart-open')
+    if (cartClass === 'cart-open') setCartClass('cart-closed')
+  }
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -44,8 +50,8 @@ export default function Nav() {
             </a>
           </li> */}
           <li>
-            <a className="main-nav-link" href="#section-testimonials">
-              Feedback
+            <a className="main-nav-link" href="#about">
+              Sobre Nosotros
             </a>
           </li>
           <li>
@@ -54,23 +60,30 @@ export default function Nav() {
             </a>
           </li>
           <li>
-            <a className="main-nav-link" href="#about">
-              Sobre Nosotros
+            <a className="main-nav-link" href="#section-testimonials">
+              Feedback
             </a>
           </li>
+          
           <li>
             {/* <Link to={"/admindashboard"} className="main-nav-link nav-cta">
               Administrar
             </Link> */}
           </li>
           <li>
-            <div className="buttonCart">
+            <div className="buttonCart" onClick={handleCartClick}>
             </div>
           </li>
         </ul>
       </nav>
 
+      <div className={cartClass}>
+            <Cart className='cart-nav'/>
+            <button className="close-cart" onClick={handleCartClick}>X</button>
+      </div>
+
       {/*  <button className="btn-mobile-nav" >{!mobileNav ? 'Open' : 'Close'}</button> */}
     </header>
+    
   );
 }
