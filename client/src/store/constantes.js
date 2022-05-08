5fee8bf31e3bdab816f0722de9bdc34b28dec595
Santi <juan_santiago@outlook.com>
2022-05-07 23:52:37 -0300

    se agregan costantes para despachar order, se edita checkout, despacha post de orders

---
+++ client/src/store/constantes.js
@@ -0,0 +1,5 @@
+
+export const BASE_URL = 'https://pfgymapp-2.herokuapp.com';
+export const USER_LOAD = BASE_URL + 'users/';
+export const ORDERS_URL = BASE_URL + 'orders/';
+export const POST_MERCADOPAGO = BASE_URL + 'mercadopago/'
