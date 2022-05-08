7e697e650ff0ad2e5735aac9e67b4eebc2578e5e
gadeangelis <gadeangelis@gmail.com>
2022-05-07 21:03:49 -0300

    paginado
    paginado terminado

---
+++ client/src/views/SocioPage/components/Recetas/Paginado.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import style from './Paginado.module.css'
+
+export default function Paginado({cardsPerPage, dogs, paginado}){
+
+    const pageNumber = []
+    for (let i=0; i < Math.ceil(dogs / cardsPerPage); i++){
+        pageNumber.push(i+1)
+    }
+    return (
+        <nav>
+            <ul className= {style.paginado}>
+                {
+                    pageNumber?.map(number => (
+                        <li key={number}>
+                        <button className= {style.botonp} onClick={() => paginado(number)}>{number}</button>
+                        </li>
+                    ))   
+                }
+            </ul>
+        </nav>
+    )
+       
+
+    
+}
--- client/src/views/SocioPage/components/Recetas/Recetas.jsx
+++ client/src/views/SocioPage/components/Recetas/Recetas.jsx
@@ -2,15 +2,20 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getRecipes } from "../../store/actions-recipes/recipes";
 import SearchBar from "../../components/SearchRecipes/SearchBar";
+import Paginado from "./components/Paginado/Paginado";
 
 export default function SocioPage() {
     const dispatch = useDispatch();
     const allRecipes = useSelector((state) => state.recipes);
     const [currentPage, setCurrentPage] = useState(1)
-    const [recipesPerPage, setRecipesPerPage] = useState(9)
+    const [recipesPerPage, setRecipesPerPage] = useState(12)
     const indexOfLastRecipe = currentPage * recipesPerPage
     const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage
     const currentRecipes = allRecipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+    const paginado = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
 
 
 
@@ -38,6 +43,16 @@ function handleClick(e){
     allRecipes={allRecipes.length} 
     paginado={paginado} />        
 
+    <span>
+{  
+    currentRecipes?.map((el) => {
+        return(
+        <div key={el.id}>
         </div>
+        )
+    })
+}
+    </span>
+</div>
 );
 }
