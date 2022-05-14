67d074810e310f3894fb23f4bf2c09018055e31b
Yanina <yaninagarcia.ana@gmail.com>
2022-05-13 21:42:49 -0300

    cambio

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -4,8 +4,6 @@ import {
   RECEIVED_POST,
   REQUEST_POST,
   GET_ALL_USERS,
-  GET_USER_BY_EMAIL,
-  CREATE_USER,
 } from "../actions-type/index";
 
 import { BASE_URL } from "../constantes";
@@ -159,30 +157,7 @@ export function changePage(page) {
   };
 }
 
-const getUserByEmail = (email)=> {
-  return async function (dispatch){
-    try {
-      const {data} = await axios.get(`${BASE_URL}/user/`, email)
-      return dispatch({
-        type: GET_USER_BY_EMAIL,
-        payload:data
-    })
-    } catch (error) {
-      console.log("getUserByEmail", error)
-    }
-  }
-}
 
-const createNewUser = (payload)=> {
-  return async function (){
-    try {
-      const newUser = await axios.post(`${BASE_URL}/user`, payload)
-      return newUser
-    } catch (error) {
-      console.log("createNewUser", error)
-    }
-  }
-}
 
 export {
   createActivity,
@@ -192,6 +167,4 @@ export {
   editActivity,
   requestPost,
   getAllUsers,
-  getUserByEmail,
-  createNewUser,
 };
