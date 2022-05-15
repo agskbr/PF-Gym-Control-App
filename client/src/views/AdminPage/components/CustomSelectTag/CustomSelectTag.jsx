5d4bbc71d47b4a118294c0e09a73dae6267b0cbe
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-05-15 20:17:29 -0300

    Panel de admin permitiendo crear: Clases, instructores y dias-horas. Permitiendo editar Clases.

--- client/src/views/AdminPage/components/CustomSelectTag/CustomSelectTag.jsx
+++ client/src/views/AdminPage/components/CustomSelectTag/CustomSelectTag.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import style from "./CustomSelectTag.module.css";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { validateForm } from "../../../../utils/validateForm";
+import { useDispatch } from "react-redux";
+import { deleteDayHourFromActivity } from "../../../../store/actions";
 
 export default function CustomSelectTag({
   handlerChangeSelectTag,
@@ -15,6 +17,7 @@ export default function CustomSelectTag({
   inputs,
   type,
 }) {
+  const dispatch = useDispatch();
   const [displayItems, setDisplayItems] = useState([]);
   useEffect(() => {
     setDisplayItems([...visualizeItems]);
@@ -27,40 +30,76 @@ export default function CustomSelectTag({
         name={name}
       >
         <option hidden>{firstOpt}</option>
-        {options.map((opt) => (
-          <option key={opt.id} value={opt.name}>
-            {opt.name}
-          </option>
-        ))}
+        {options.map((opt) =>
+          !opt.activityId ? (
+            <option
+              key={opt.id}
+              value={
+                !opt.day
+                  ? `${opt.name} (${opt.id})`
+                  : `${opt.day}-${opt.hour} (${opt.id})`
+              }
+            >
+              {!opt.day ? `${opt.name} (${opt.id})` : `${opt.day}-${opt.hour}`}
+            </option>
+          ) : null
+        )}
       </select>
       <div className={style.selectedItemsContainer}>
         <ul>
-          {displayItems.map((item, i) => (
-            <div key={i} className={style.itemContainer}>
-              <li key={item}>{item}</li>
-              <AiFillCloseCircle
-                key={Math.random()}
-                onClick={() => {
-                  setDisplayItems((state) => state.filter((e) => e !== item));
-                  setInputs((state) => ({
-                    ...state,
-                    [name]: visualizeItems.filter((e) => e !== item),
-                  }));
-                  setErrors(
-                    validateForm(
-                      {
-                        ...inputs,
-                        [name]: visualizeItems.filter((e) => e !== item),
-                      },
-                      type
-                    )
-                  );
-                }}
-                color="red"
-                className={style.deleteItemIcon}
-              />
-            </div>
-          ))}
+          {displayItems.map((item, i) => {
+            return (
+              <div key={i} className={style.itemContainer}>
+                <li key={item.id}>
+                  {item.day ? `${item.day}-${item.hour}` : item}
+                </li>
+                <AiFillCloseCircle
+                  key={Math.random()}
+                  onClick={() => {
+                    setDisplayItems((state) =>
+                      state.filter((e) => {
+                        if (e.id) {
+                          dispatch(
+                            deleteDayHourFromActivity(item.activityId, item.id)
+                          );
+                          return e.id !== item.id;
+                        } else {
+                          return e !== item;
+                        }
+                      })
+                    );
+                    setInputs((state) => ({
+                      ...state,
+                      [name]: visualizeItems.filter((e) => {
+                        if (e.id) {
+                          return e.id !== item.id;
+                        } else {
+                          return e !== item;
+                        }
+                      }),
+                    }));
+                    setErrors(
+                      validateForm(
+                        {
+                          ...inputs,
+                          [name]: visualizeItems.filter((e) => {
+                            if (e.id) {
+                              return e.id !== item.id;
+                            } else {
+                              return e !== item;
+                            }
+                          }),
+                        },
+                        type
+                      )
+                    );
+                  }}
+                  color="red"
+                  className={style.deleteItemIcon}
+                />
+              </div>
+            );
+          })}
         </ul>
       </div>
       <label htmlFor={name}>{errorLabel}</label>
