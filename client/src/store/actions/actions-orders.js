f05ae6e2c39121fcd69f329e29d72423b5ce3648
Yanina <yaninagarcia.ana@gmail.com>
2022-05-15 13:54:31 -0300

    agrego actions para las ordenes en action-orders

--- client/src/store/actions/actions-orders.js
+++ client/src/store/actions/actions-orders.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { RECEIVED_POST, GET_ALL_ORDERS } from "../actions-type/index";
+import { 
+  RECEIVED_POST,
+  GET_ALL_ORDERS, 
+  GET_ALL_ORDERS_BY_USER, 
+  GET_ORDERLINE_BY_ORDER_ID 
+} from "../actions-type/index";
 import { BASE_URL } from "../constantes";
 
 const getAllOrders = () => {
@@ -15,4 +20,38 @@ const getAllOrders = () => {
   };
 };
 
-export { getAllOrders };
+const getAllOrdersByUser = (id) => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.get(`${BASE_URL}/order/user/${id}`)
+      return dispatch({
+        type:GET_ALL_ORDERS_BY_USER,
+        payload: data
+      })
+    } catch (error) {
+      console.log("ordersByUser",error)
+    }
+  }
+}
+
+const getOrderlineByOrderid = (orderId) => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.get(`${BASE_URL}/orderline/${orderId}`)
+      console.log("data", data)
+      return dispatch({
+        type: GET_ORDERLINE_BY_ORDER_ID,
+        payload: data
+      })
+    } catch (error) {
+      console.log("orderlineByOrderid", error)
+    }
+  }
+}
+
+
+export { 
+  getAllOrders,
+  getAllOrdersByUser,
+  getOrderlineByOrderid
+};
