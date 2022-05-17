5d505438a9044bc36e8cc79f1b82a32cc16e56ce
Eugenia Nannini <euge.nannini@gmail.com>
2022-05-17 00:32:22 -0300

    cambios en activity
    my act, socio page

--- client/src/views/SocioPage/SocioPage.jsx
+++ client/src/views/SocioPage/SocioPage.jsx
@@ -1,11 +1,18 @@
 import style from "./SocioPage.module.css";
 import SideBar from "./components/Sidebar/SideBar";
 import CenterDynamicView from "./components/CenterDynamicView/CenterDynamicView";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getActivity } from "../../store/actions/index";
+import { getOrderlineByOrderid, getAllOrdersByUser } from "../../store/actions/actions-orders";
+import { useSelector } from "react-redux";
+import { getUserById } from "../../store/actions/actions-user";
+import { useDispatch } from "react-redux";
 
 
 export default function SocioPage() {
   const [itemSelected, setItemSelected] = useState("Mi Perfil");
+
+
 
   return (
     <div className={style.principalContainer}>
